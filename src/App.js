ce1647443cd9a66fa78159a744dc364e6d00af43
Jeremy R <lvlgrinddesign@gmail.com>
2019-04-03 08:52:24 -0400

    added styling to test scroll function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
+
+import Header from './components/header_footer/header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        hello
+      <div className="App" style={{height: '1000px', background:'cornFlowerBlue'}}>
+        <Header />
       </div>
     );
   }
