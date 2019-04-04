38e49f0f542ac74a4482d92b7919729a5a5d90c1
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-04 11:10:54 -0400

    imported new components into index.js

---
+++ src/components/highlights/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Desc from './desc';
+import Discount from './discount';
+
+const Highlights = () => {
+    return (
+        <div className="highlight_wrapper">
+            <Desc />
+            <Discount />
+        </div>
+    );
+};
+
+export default Highlights;
