dca88917d5a945395c4765e47977511ee89af0f7
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-04 09:39:22 -0400

    added delay to the timer on page load

--- src/components/featured/countdown.js
+++ src/components/featured/countdown.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Slide from 'react-reveal/Slide';
+
 
 export default class countdown extends Component {
     state = {
@@ -7,45 +9,47 @@ export default class countdown extends Component {
 
     render() {
         return (
-        <div className="countdown_wrapper">
-            <div className="countdown_top">
-                Event Starts In
+        <Slide left delay={1000}>
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event Starts In
+                </div>
+                <div className="countdown_bottom">
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            23
+                        </div>
+                        <div className="countdown_tag">
+                            days
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            2
+                        </div>
+                        <div className="countdown_tag">
+                            HS
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            20
+                        </div>
+                        <div className="countdown_tag">
+                            Min
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            40
+                        </div>
+                        <div className="countdown_tag">
+                            sec
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="countdown_bottom">
-                <div className="countdown_item">
-                    <div className="countdown_time">
-                        23
-                    </div>
-                    <div className="countdown_tag">
-                        days
-                    </div>
-                </div>
-                <div className="countdown_item">
-                    <div className="countdown_time">
-                        2
-                    </div>
-                    <div className="countdown_tag">
-                        HS
-                    </div>
-                </div>
-                <div className="countdown_item">
-                    <div className="countdown_time">
-                        20
-                    </div>
-                    <div className="countdown_tag">
-                        Min
-                    </div>
-                </div>
-                <div className="countdown_item">
-                    <div className="countdown_time">
-                        40
-                    </div>
-                    <div className="countdown_tag">
-                        sec
-                    </div>
-                </div>
-            </div>
-        </div>
+        </Slide>
         )
     }
 }
