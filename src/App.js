bc8573fc81828bc632497148247d5f42b19a8fa6
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-04 09:32:43 -0400

    imported

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/header';
-
+import Featured from './components/featured';
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: '1000px', background:'cornFlowerBlue'}}>
         <Header />
+        <Featured />
       </div>
     );
   }
