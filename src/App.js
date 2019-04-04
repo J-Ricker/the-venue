b5aec63ec803f2e5dc100fc8ed42fb9c4797f59b
J-Ricker <lvlgrinddesign@gmail.com>
2019-04-04 10:17:48 -0400

    imported venue info into app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './resources/styles.css';
 
 import Header from './components/header_footer/header';
 import Featured from './components/featured';
+import VenueInfo from './components/venue-info';
+
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: '1000px', background:'cornFlowerBlue'}}>
         <Header />
         <Featured />
+        <VenueInfo />
       </div>
     );
   }
